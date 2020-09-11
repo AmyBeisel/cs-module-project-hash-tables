77e122270a55c1ffacfa1bb2e7da15d577ca3f27
Amy Beisel <amy.beisel@gmail.com>
2020-09-10 23:03:32 -0600

    Day 3

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,12 +1,22 @@
 def word_count(s):
-    name = s.replace('"', '').replace(':', '').replace(';','').replace(',','').replace('.','').replace('-','').replace('+','').replace('=','').replace('/','').replace('|','').replace('[', '').replace(']','').replace('{','').replace('}','').replace('(','').replace(')','').replace('*','').replace('^','').replace('&','').replace('\\','')
-    if name == '':
-        return {}
-    split = name.lower().split()
-    words = {i:split.count(i) for i in split}
-    return words
+    # name = s.replace('"', '').replace(':', '').replace(';','').replace(',','').replace('.','').replace('-','').replace('+','').replace('=','').replace('/','').replace('|','').replace('[', '').replace(']','').replace('{','').replace('}','').replace('(','').replace(')','').replace('*','').replace('^','').replace('&','').replace('\\','')
+    # if name == '':
+    #     return {}
+    # split = name.lower().split()
+    # words = {i:split.count(i) for i in split}
+    # return words
 
-
+    dict = {}
+    ignoreChars = '" : ; , . - + = / \\ | [ ] { } ( ) * ^ &'.split()
+    lower_s = s.lower()
+    for i in ignoreChars:
+        lower_s = lower_s.replace(i, " ")
+    for word in lower_s.split():
+        if word not in dict:
+            dict[word] = 1
+        else:
+            dict[word] += 1
+    return dict
 
 if __name__ == "__main__":
     print(word_count(""))
